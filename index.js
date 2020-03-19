81e6e0885b447666a21f7752bd272b6de65fd915
Christian Cain <christian22cain@yahoo.com>
2020-03-19 15:46:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+// TEST 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// TEST 2
+let random = Math.ceil(Math.random() * 10) + 1;
+
+// TEST 3
+let mod = (6 * 4) % 5;
+
+// TEST 4
+let max = Math.max(20, 10, 3, 18, 7);
