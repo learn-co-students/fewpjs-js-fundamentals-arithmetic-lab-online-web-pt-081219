789548e1f8f739b67178cd2b14ce1d752f314848
JCGuest <jcguest90@gmail.com>
2020-03-22 01:21:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+var num1 = 6.2
+var num2 = 10
+var multiply = num1 * num2
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) + 1
+
+var mod = 16 % 6
+
+var max = Math.max(1,2,3,4,5,20)
