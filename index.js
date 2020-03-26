5c0221fd6fa9ac5114a21169857cc47de5240410
saramccombs <theSaraMcCombs@gmail.com>
2020-03-26 12:41:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+// Assignment part 1
+// Create a variable called multiply that will multiply
+// the variables num1 and num2 whose results will equal "62".
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+// Assignment part 2
+// Create a variable called random that will generate a
+// random integer greater than 0.
+const random = Math.random()
+
+// Assignment part 3
+// Create a variable called mod set to an equation
+// whose remainder is equal to "4".
+let num3 = 124
+let num4 = 10
+const mod = num3 % num4
+
+// Assignment part 4
+// Create a variable called max that will return "20"
+// as the highest number in the set.
+const max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
