9804510b891bd0131908178be191b5eb80d173ba
kierra <51520037+kierrakay@users.noreply.github.com>
2020-03-28 15:28:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2 
+let random = Math.floor(Math.random()*1000) + 1
+let max = Math.max( 4,6,8,20)
+const mod = 16 % 6
